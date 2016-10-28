fcd17cf134695a6b1c10a255380c40665f130959
hidysea <hidysea@163.com>
2016-10-28 18:14:47 +0800

    jekyll install build and serve

---
+++ _site/js/newwindow.js
@@ -0,0 +1,22 @@
+/**
+ * Created by HidyChen on 16/10/28.
+ */
+/*
+ function openInNewWindow() {
+ var newWindow = window.open(this.getAttribute('href'), '_blank');
+ newWindow.focus();
+ return false;
+ }
+ */
+
+if (document.getElementById) {
+    var newWindowLink;
+    var newWindowLinks = document.getElementById('main-content').getElementsByTagName('a');
+    for (var i = 0; i < newWindowLinks.length; i++) {
+        newWindowLink = newWindowLinks[i];
+        if (!((newWindowLink.href.indexOf("ruanyifeng") != -1) && (newWindowLink.href.indexOf("#") != -1)) ){
+            // newWindowLink.onclick = openInNewWindow;
+            newWindowLink.target = '_blank';
+        }
+    }
+}
